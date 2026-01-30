aaf4e2a8b1e6d6b40cc846665ac3152dc3e9ce77
Dongwon Henry Seon <123602432+boolYikes@users.noreply.github.com>
2026-01-30 09:30:30 +0000

    Exercise 11.10 - 11.12 Render, unhealthy revision test

--- app.js
+++ app.js
@@ -7,10 +7,16 @@ const PORT = process.env.PORT || 5000
 app.use(express.static('dist'))
 
 app.get('/version', (req, res) => {
-  res.send('2') // change this string to ensure a new version deployed
+  // 1: init
+  // 2: the one with deploy hook
+  // 3: the non-healthy one
+  // 4: back to the correct one
+  res.send('3') // change this string to ensure a new version deployed
 })
 
 app.get('/healthz', (req, res) => {
+  // eslint-disable-next-line no-constant-condition
+  if (true) throw('Unhealthy bumpkin')
   res.status(200).send('ok')
 })
 
